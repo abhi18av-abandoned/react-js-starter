8f76345bef9387df71d7102c638b23babd416f0b
Abhinav Sharma <abhi18av@users.noreply.github.com>
2019-11-23 15:10:49 +0530

    return a fragment from the main App component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">Master React</header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">Master React</header>
+      </div>
+    </>
   );
 };
